b270a1a09acc9ecf5475280ebac1970cfea0099e
Wendy Grapentine <wendygrapentine@gmail.com>
2017-07-21 16:35:35 -0700

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router"; // This link (see below) links to the header on the pg so that when user clicks
-                                      //on the title it takes them back to the search bar page.
+//on the title it takes them back to the search bar page.
 
 class App extends Component {
   render() {
     return (
-     <div className="main-app">
-         <header className="main-header">
-           <h1>
-              <Link to="/">React Git Searchin'</Link> 
-           </h1>
-         </header>
+      <div className="main-app">
+        <header className="main-header">
+          <h1>
+            <Link to="/">React Git Searchin'</Link>
+          </h1>
+        </header>
         <main className="main-content">
-        {this.props.children}
+          {this.props.children}
         </main>
       </div>
     );
