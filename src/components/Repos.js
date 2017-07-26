df73904449b24220577fceb1d2ec16c0f3a04e60
Wendy Grapentine <wendygrapentine@gmail.com>
2017-07-26 13:53:26 -0700

    repos access key =

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -18,7 +18,7 @@ class Repos extends Component {
       var that = this;
       var page = this.state.page;
 
-    fetch(`https://api.github.com/users/${this.props.params.username}/repos?access_token554612bc8de7a1a6744b77055cbab693543d20f0`)  
+    fetch(`https://api.github.com/users/${this.props.params.username}/repos?access_token=554612bc8de7a1a6744b77055cbab693543d20f0`)  
       .then(response => response.json())
       .then(repos => {
           if (repos.length === 0) {
