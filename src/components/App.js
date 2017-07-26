6cb0686646f6f8d225d504ade12a5dea9fc9dd6f
Wendy Grapentine <wendygrapentine@gmail.com>
2017-07-26 10:55:00 -0700

    IT IS DONE(FOR NOW)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router"; // This link (see below) links to the header on the pg so that when user clicks
 //on the title it takes them back to the search bar page.
-
+//This is the layout component. It's desplayed by the top-level Route
+//this.props.children will correspond to the current URL's component.
+//If the URL is only / then the IndexRoute's component will be the child (Search component)
+//If the URL is /user/:username then the User component will be displayed.
 class App extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import{ Router, Route, IndexRoute, browserHistory } from "react-router";
+import App from './components/App';
 import Search from "./components/Search";
 import User from "./components/User";
+import Followers from "./components/Followers";
+import Following from "./components/Following";
+import Repos from "./components/Repos";
+
 
 
 //create some routes yo! // Search is a child of app  //:username means we are going to use it as a parameter
@@ -12,7 +16,11 @@ const routes = (
     <Router history={browserHistory}>
       <Route path="/" component={App}>   
          <IndexRoute component={Search} />  
-         <Route path="user/:username" component={User} />
+         <Route path="user/:username" component={User}>
+           <Route path="followers" component={Followers} />
+           <Route path="following" component={Following} />
+           <Route path="repos" component={Repos} />
+         </Route> 
       </Route>
     </Router>
 );
